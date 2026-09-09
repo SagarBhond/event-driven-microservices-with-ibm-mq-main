ea125f32a1d2d701e28cbafdf7c1d50b9421e92f
Sagar Bhond <YOUR_GITHUB_EMAIL>
2026-09-09 17:38:58 +0530

    Manage GitHub IAM role outside Terraform

--- terraform/github_actions.tf
+++ terraform/github_actions.tf
@@ -1,27 +1,3 @@
-data "aws_iam_policy_document" "github_actions_assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:SagarBhond/event-driven-microservices-with-ibm-mq-main:ref:refs/heads/main"]
-    }
-  }
-}
-
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
@@ -36,49 +12,7 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   }
 }
 
-resource "aws_iam_role" "github_actions" {
-  name               = "github-actions-order-saga-role"
-  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
-}
-
-resource "aws_iam_role_policy" "github_actions_deploy" {
-  name = "github-actions-order-saga-deploy"
-  role = aws_iam_role.github_actions.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:CompleteLayerUpload",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-        Resource = "arn:aws:ecr:${var.aws_region}:${data.aws_caller_identity.current.account_id}:repository/order-saga-*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecs:DescribeServices",
-          "ecs:UpdateService"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
 output "github_actions_role_arn" {
   description = "Role ARN to store in the GitHub AWS_ROLE_ARN secret"
-  value       = aws_iam_role.github_actions.arn
+  value       = "arn:aws:iam::882040517001:role/github-actions-order-saga-role"
 }
